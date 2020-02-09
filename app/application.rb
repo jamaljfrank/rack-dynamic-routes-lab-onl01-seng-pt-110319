c093349a78b3887b20142b9825d2e851590e637d
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 03:33:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,18 @@
 class Application
  
+  @@songs = [Song.new("Sorry", "Justin Bieber"),
+            Song.new("Hello","Adele")]
+ 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
-    if req.path=="/songs"
-      resp.write "You requested the songs"
-    else
-      resp.write "Route not found"
-      resp.status = 404
+    if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
     end
  
     resp.finish
