af5fa28c6a645b991a28457199052c35ce428869
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 04:03:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,15 +3,16 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path.match(/items/)
- 
-      item = req.path.split("/items/").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
- 
-      resp.write song.artist
-    end
- 
-    resp.finish
-  end
-end
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else 
+        @@cart.each do |i|
+          resp.write "#{i}\n"
+        end
+      
