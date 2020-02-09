c1a22d80fea7d4002fb7c9361dbfeaf258f47ba4
Jamal Frank <jamaljfrank@gmail.com>
2020-02-09 04:16:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|s| s.name == item_name}
-      if item
+      if item,
         resp.write item.price
       else 
       resp.write "Route not found"
